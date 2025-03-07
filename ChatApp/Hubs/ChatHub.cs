b5492bc1fcc471fe791c352fde53efe231d5924a
KirlosNabil <kirlosnabil956@gmail.com>
2025-03-07 16:19:39 +0200

    Added and Implemented ChatHub Class

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        private static ConcurrentDictionary<string, string> _connectedUsers = new ConcurrentDictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;
+            if (userId != null)
+            {
+                _connectedUsers[userId] = Context.ConnectionId;
+            }
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var userId = Context.UserIdentifier;
+            if (userId != null)
+            {
+                _connectedUsers.TryRemove(userId, out _);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+        public async Task SendMessage(string receiverId, string message)
+        {
+            var senderId = Context.UserIdentifier;
+            if (senderId != null && _connectedUsers.TryGetValue(receiverId, out var receiverConnectionId))
+            {
+                await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", senderId, message);
+            }
+        }
+    }
+}
