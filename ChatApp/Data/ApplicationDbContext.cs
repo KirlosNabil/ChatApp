30f63afa96818b79ba6538c2ba44e7fa8eaa855b
KirlosNabil <kirlosnabil956@gmail.com>
2025-03-07 16:05:43 +0200

    Added User Class

--- ChatApp/Data/ApplicationDbContext.cs
+++ ChatApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
