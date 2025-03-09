36f9ee1bccc2a0e9342bfa7064492793a1ffcdcd
KirlosNabil <kirlosnabil956@gmail.com>
2025-03-09 06:45:40 +0200

    Added searching for friends feature

--- ChatApp/Controllers/HomeController.cs
+++ ChatApp/Controllers/HomeController.cs
@@ -1,16 +1,41 @@
 using System.Diagnostics;
+using ChatApp.Data;
 using ChatApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Azure.Core.HttpHeader;
 
 namespace ChatApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _dbContext;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Search(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return View("Index", new List<User>());
+            }
+
+            var names = username.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var users = new List<User>();
+            if (names.Length == 1)
+            {
+                users  = _dbContext.Users.Where(u => u.FirstName.Contains(names[0])).ToList();
+            }
+            else if (names.Length >= 2)
+            {
+                users = _dbContext.Users.Where(u => u.FirstName.Contains(names[0]) && u.LastName.Contains(names[1])).ToList();
+            }
+            return View("Index", users);
         }
 
         public IActionResult Index()
