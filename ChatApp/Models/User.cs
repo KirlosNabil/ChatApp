7590415f63a74b016a53ebeeae2cbb5d096678ee
KirlosNabil <kirlosnabil956@gmail.com>
2025-03-09 04:32:10 +0200

    Updated User Model
    Added friend list, friend requests, first and last name, phone number

--- ChatApp/Models/User.cs
+++ ChatApp/Models/User.cs
@@ -4,5 +4,10 @@ namespace ChatApp.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber {  get; set; }
+        public List<string> FriendList { get; set; }
+        public List<string> FriendRequests { get; set; }
     }
 }
